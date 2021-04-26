0d191fd52cf1cc4371050b00fcd7cc2814d690ba
nidhiverma1997 <nidhiverma1997@gmail.com>
2021-04-27 02:41:56 +0530

    Add section specific markdown code

--- src/App.js
+++ src/App.js
@@ -1,116 +1,225 @@
 import './App.css';
-import { useState } from 'react';
-import { Navbar } from './components/Navbar';
-import { Attribution } from './components/Attribution';
+import { Fragment, useReducer, useEffect, useState } from 'react';
 import MDEditor from '@uiw/react-md-editor';
-
-const mkdStr = `# Markdown Editor for React
-
-**Hello world!!!**
-
-\`\`\`javascript
-import React from "react";
-import ReactDOM from "react-dom";
-import MEDitor from '@uiw/react-md-editor';
-
-export default function App() {
-  const [value, setValue] = React.useState("**Hello world!!!**");
-  return (
-    <div className="container">
-      <MEDitor
-        value={value}
-        onChange={setValue}
-      />
-      <MDEditor.Markdown source={value} />
-    </div>
-  );
-}
-\`\`\`
-
-`;
+import templateStrings from './templateStrings';
 
 function App() {
-  const [value, setValue] = useState(mkdStr);
+  const initialState = {
+    value: '',
+    sectionsArray: [],
+    output: '',
+    sections: [],
+  };
 
+  const [togglePreview, setTogglePreview] = useState(true);
+  function stateReducer(state, action) {
+    switch (action.type) {
+      case 'updateValue':
+        return {
+          ...state,
+          value: action.payload,
+        };
+      case 'updateArray':
+        return {
+          ...state,
+          sectionsArray: [...state.sectionsArray, state.value],
+        };
+      case 'updateOutput':
+        return {
+          ...state,
+          output: state.sectionsArray.join('\n'),
+        };
+      case 'updateSections':
+        return {
+          ...state,
+          sections: [...state.sections, action.payload],
+        };
+      default:
+        throw new Error();
+    }
+  }
+
+  const [state, dispatch] = useReducer(stateReducer, initialState);
+
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
   return (
-    <div className='App'>
-      <Navbar />
-      <div className='container'>
-        <section className='sidebar'>
-          <h4>Options</h4>
-          <div>
-            <button
-              className='btn'
-              onClick={() => {
-                setValue(value + `\n# Documentation`);
-              }}
-            >
-              Documentation
-            </button>
-            <button className='btn'>Contributing</button>
-            <button className='btn'>API</button>
-            <button className='btn'>Acknowledgements</button>
-            <button className='btn'>License</button>
-          </div>
-        </section>
-        <section>
-          <h4>Editor</h4>
+    <Fragment>
+      <div className='grid grid-cols-5 gap-4 pt-2'>
+        <div className='pl-8'>
+          <h4 className='py-4 text-blue-500 font-semibold'>Sections</h4>
+        </div>
+        <div className='col-span-2'>
+          <h4 className='py-4 text-blue-500 font-semibold'>Editor</h4>
+        </div>
+        <div className='col-span-2'>
+          <button
+            className='py-4 text-blue-500 font-semibold outline-none cursor-pointer'
+            onClick={() => setTogglePreview(true)}
+          >
+            Preview
+          </button>
+          <button
+            className='pl-4 py-4 text-gray-500 font-semibold outline-none cursor-pointer'
+            onClick={() => setTogglePreview(false)}
+          >
+            Raw
+          </button>
+        </div>
+      </div>
+      <div className='grid grid-cols-5 gap-4'>
+        <div className='flex w-full h-3/4 px-8 pt-4 overflow-y-scroll'>
+          <ul className='list-none space-y-4 w-full h-full'>
+            <li>
+              <button
+                className='bg-white px-4 py-2 w-full shadow cursor-pointer rounded-md'
+                onClick={() => {
+                  dispatch({
+                    type: 'updateValue',
+                    payload: templateStrings.documentation,
+                  });
+
+                  dispatch({ type: 'updateArray' });
+                  dispatch({ type: 'updateOutput' });
+                  dispatch({
+                    type: 'updateSections',
+                    payload: 'documentation',
+                  });
+                }}
+              >
+                Documentation
+              </button>
+            </li>
+            <li>
+              <button
+                className='bg-white px-4 py-2 w-full shadow rounded-md cursor-pointer'
+                onClick={() => {
+                  dispatch({
+                    type: 'updateValue',
+                    payload: templateStrings.contributing,
+                  });
+                  dispatch({ type: 'updateArray' });
+                  dispatch({ type: 'updateOutput' });
+                  dispatch({
+                    type: 'updateSections',
+                    payload: 'contributing',
+                  });
+                }}
+              >
+                Contributing
+              </button>
+            </li>
+            <li>
+              <button
+                className='bg-white px-4 py-2 w-full shadow rounded-md cursor-pointer'
+                onClick={() => {
+                  dispatch({
+                    type: 'updateValue',
+                    payload: templateStrings.apiReference,
+                  });
+                  dispatch({ type: 'updateArray' });
+                  dispatch({ type: 'updateOutput' });
+                  dispatch({
+                    type: 'updateSections',
+                    payload: 'apiReference',
+                  });
+                }}
+              >
+                API Reference
+              </button>
+            </li>
+            <li>
+              <button
+                className='bg-white px-4 py-2 w-full shadow rounded-md cursor-pointer'
+                onClick={() => {
+                  dispatch({
+                    type: 'updateValue',
+                    payload: templateStrings.acknowledgements,
+                  });
+                  dispatch({ type: 'updateArray' });
+                  dispatch({ type: 'updateOutput' });
+                  dispatch({
+                    type: 'updateSections',
+                    payload: 'acknowledgements',
+                  });
+                }}
+              >
+                Acknowledgements
+              </button>
+            </li>
+            <li>
+              <button
+                className='bg-white shadow px-4 py-2 w-full rounded-md cursor-pointer'
+                onClick={() => {
+                  dispatch({
+                    type: 'updateValue',
+                    payload: templateStrings.license,
+                  });
+                  dispatch({
+                    type: 'updateArray',
+                  });
+                  dispatch({ type: 'updateOutput' });
+                  dispatch({ type: 'updateSections', payload: 'license' });
+                }}
+              >
+                License
+              </button>
+            </li>
+            <li>
+              <button
+                className='bg-white px-4 py-2 w-full shadow rounded-md cursor-pointer'
+                onClick={() => {
+                  dispatch({
+                    type: 'updateValue',
+                    payload: templateStrings.appendix,
+                  });
+                  dispatch({ type: 'updateArray' });
+                  dispatch({ type: 'updateOutput' });
+                  dispatch({ type: 'updateSections', payload: 'appendix' });
+                }}
+              >
+                Appendix
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div className='col-span-2 h-11/12'>
           <textarea
-            className='editor'
+            className='bg-black text-white p-8 overflow-y-scroll w-full'
             autoFocus
             rows={20}
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
+            value={state.value}
+            onChange={(e) =>
+              dispatch(
+                { type: 'updateValue', payload: e.target.value },
+                { type: 'updateArray' }
+              )
+            }
           />
-        </section>
-        {/* <MDEditor height={200} value={value} onChange={setValue} /> */}
-        {/* <div style={{ padding: '50px 0 0 0' }} /> */}
-        <section>
-          <h4>Preview</h4>
-          <MDEditor.Markdown source={value} className='markdown' />
-        </section>
+        </div>
+        <div className='col-span-2 h-11/12 mr-8'>
+          {togglePreview === true ? (
+            <MDEditor.Markdown
+              source={state.output}
+              className='p-8 rounded-md w-full h-3/4 border-2 border-gray-200 overflow-y-scroll'
+            />
+          ) : (
+            <div className='p-8 rounded-md w-full h-3/4 border-2 border-gray-200 overflow-y-scroll'>
+              {state.sectionsArray.map((section, key) => (
+                <div className='pb-4'>
+                  {section.split('\n').map((s) => (
+                    <p>{s}</p>
+                  ))}
+                  <br />
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 }
 
 export default App;
-
-// function App() {
-//   const [input, setInput] = useState('');
-//   return (
-//     <div className='App'>
-//       <Navbar />
-//       <div className='container'>
-//         <Sidebar />
-//         <section>
-//           <h4>Markdown Input</h4>
-//           <textarea
-//             className='editor'
-//             autoFocus
-//             rows={20}
-//             onChange={(e) => setInput(e.target.value)}
-//           />
-//         </section>
-//         <section>
-//           <h4>Preview</h4>
-//           <ReactMarkdown
-//             className='markdown'
-//             children={input}
-//             plugins={[gfm]}
-//           />
-//           {/* <div
-//             className='markdown'
-//             dangerouslySetInnerHTML={{
-//               __html: marked(input, { breaks: true }),
-//               className="hljs"
-//             }}
-//           ></div> */}
-//         </section>
-//       </div>
-//       <Attribution />
-//     </div>
-//   );
-// }
-
-// export default App;
