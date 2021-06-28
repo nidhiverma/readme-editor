1dc087ff6cda3eb4f3b9eea94c1ba433b34af134
nidhiverma1997 <nidhiverma1997@gmail.com>
2021-06-28 14:33:59 +0530

    Update template strings

--- src/templateStrings.js
+++ src/templateStrings.js
@@ -20,11 +20,11 @@ A brief description of what this project does and who it's for`,
 
 To contribute to <project_name>, follow these steps:
 
-1. Fork this repository.
-2. Create a branch: git checkout -b <branch_name>.
-3. Make your changes and commit them: \`\`\`git commit -m <commit_message>\`\`\`
-4. Push to the original branch: \`\`\`git push origin <project_name>/<location>\`\`\`
-5. Create the pull request.
+- Fork this repository.
+- Create a branch: git checkout -b <branch_name>.
+- Make your changes and commit them: \`\`\`git commit -m <commit_message>\`\`\`
+- Push to the original branch: \`\`\`git push origin <project_name>/<location>\`\`\`
+- Create the pull request.
 
 Alternatively see the GitHub documentation on [creating a pull request](https://docs.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request).`,
 
