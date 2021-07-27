e963b5c8e43caeebdebf8415cd21c3a166219294
nidhiverma1997 <nidhiverma1997@gmail.com>
2021-07-27 17:58:16 +0530

    Remove typo

--- src/components/App.js
+++ src/components/App.js
@@ -226,7 +226,6 @@ function App() {
                       <br />
                     </div>
                   ))}
-                  /
                 </div>
               )}
             </div>
